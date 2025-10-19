8c272866f8310b278ebddaade2c89b24981ca970
Heshan Nanayakkara <kalaruheshan@gmail.com>
2025-10-19 23:52:24 +0530

    Test v3

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 # Import dependencies
 from flask import Flask
 from flask_cors import CORS
-import boto3, json, base64
-from openai import OpenAI
+import logic
 
 
 # Create Flask application
@@ -10,22 +9,14 @@ app = Flask(__name__)
 CORS(app)
 
 
-@app.route("/test", methods=["GET"])
-def test():
+@app.route("/query_dwh", methods=["GET"])
+def query_dwh():
     try:
-        client = boto3.client('secretsmanager', region_name='ap-southeast-1')
-        secret = client.get_secret_value(SecretId='rgu/research/openai')
-        creds = json.loads(secret['SecretString'])
-
-        client = OpenAI(api_key=creds['key'])
-        response = client.responses.create(
-            model="gpt-5",
-            input="Write a short bedtime story about a unicorn."
-        )
-
-        return response.output_text, 200
+        response = logic.get_query_gpt()
+        return response, 200
     except Exception as e:
         return str(e), 500
+
 
 # Run Flask application
 if __name__ == "__main__":
---
+++ logic.py
@@ -0,0 +1,16 @@
+import boto3, json
+from openai import OpenAI
+
+def get_query_gpt():
+    client = boto3.client('secretsmanager', region_name='ap-southeast-1')
+    secret = client.get_secret_value(SecretId='rgu/research/openai')
+    creds = json.loads(secret['SecretString'])
+    
+    client = OpenAI(api_key=creds['key'])
+
+    response = client.responses.create(
+        model="gpt-5",
+        input="What is the capital of France?"
+    )
+
+    return response.output_text
