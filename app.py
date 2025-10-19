2b0f8afd090ae4a3c60c96ece304aa7a03f82581
Heshan Nanayakkara <kalaruheshan@gmail.com>
2025-10-19 22:19:07 +0530

    Test function V2

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from flask import Flask
 from flask_cors import CORS
 import boto3, json, base64
+from openai import OpenAI
 
 
 # Create Flask application
@@ -15,7 +16,14 @@ def test():
         client = boto3.client('secretsmanager', region_name='ap-southeast-1')
         secret = client.get_secret_value(SecretId='rgu/research/openai')
         creds = json.loads(secret['SecretString'])
-        return creds['key'], 200
+
+        client = OpenAI(api_key=creds['key'])
+        response = client.responses.create(
+            model="gpt-5",
+            input="Write a short bedtime story about a unicorn."
+        )
+
+        return response, 200
     except Exception as e:
         return str(e), 500
 
